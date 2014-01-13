0a5b0d3aaa8ac3b50089c6d187263ed548994c79
TJ Holowaychuk <tj@vision-media.ca>
2014-01-13 06:21:38 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+
+/**
+ * Expose `Delegator`.
+ */
+
+module.exports = Delegator;
+
+/**
+ * Initialize a delegator.
+ *
+ * @param {Object} proto
+ * @param {String} target
+ * @api public
+ */
+
+function Delegator(proto, target) {
+  if (!(this instanceof Delegator)) return new Delegator(proto, target);
+  this.proto = proto;
+  this.target = target;
+  this.methods = [];
+  this.getters = [];
+  this.setters = [];
+}
+
+/**
+ * Delegate method `name`.
+ *
+ * @param {String} name
+ * @return {Delegator} self
+ * @api public
+ */
+
+Delegator.prototype.method = function(name){
+  var proto = this.proto;
+  var target = this.target;
+  this.methods.push(name);
+
+  proto[name] = function(){
+    return this[target][name].apply(this, arguments);
+  };
+
+  return this;
+};
+
+/**
+ * Delegator accessor `name`.
+ *
+ * @param {String} name
+ * @return {Delegator} self
+ * @api public
+ */
+
+Delegator.prototype.access = function(name){
+  var proto = this.proto;
+  var target = this.target;
+  this.getters.push(name);
+  this.setters.push(name);
+
+  Object.defineProperty(proto, name, {
+    get: function(){
+      return this[target][name];
+    },
+
+    set: function(val){
+      this[target][name] = val;
+    }
+  })
+
+  return this;
+};
+
+/**
+ * Delegator getter `name`.
+ *
+ * @param {String} name
+ * @return {Delegator} self
+ * @api public
+ */
+
+Delegator.prototype.getter = function(name){
+  var proto = this.proto;
+  var target = this.target;
+  this.getters.push(name);
+
+  Object.defineProperty(proto, name, {
+    get: function(){
+      return this[target][name];
+    }
+  })
+
+  return this;
+};
+
+/**
+ * Delegator setter `name`.
+ *
+ * @param {String} name
+ * @return {Delegator} self
+ * @api public
+ */
+
+Delegator.prototype.setter = function(name){
+  var proto = this.proto;
+  var target = this.target;
+  this.setters.push(name);
+
+  Object.defineProperty(proto, name, {
+    set: function(val){
+      this[target][name] = val;
+    }
+  })
+
+  return this;
+};
